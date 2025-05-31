a6459da55beff6a0ff0b962188f7df0691818ef9
Tebo12 <144923146+Tebo12@users.noreply.github.com>
2025-05-31 16:01:31 +0500

    forms again

---
+++ minesweeper/minesweeper_app/forms.py
@@ -0,0 +1,18 @@
+# from django import forms
+# from django.core.exceptions import ValidationError
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+#
+# class RegisterForm(UserCreationForm):
+#     email = forms.EmailField(required=True)
+#     class Meta:
+#         model = User
+#         fields = ("username", "email", "password1", "password2")
+#
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         password1 = cleaned_data.get("password1")
+#         password2 = cleaned_data.get("password2")
+#         if password1 and password2 and password1 != password2:
+#             raise ValidationError("Пароли не совпадают")
+#         return cleaned_data
--- minesweeper/minesweeper_app/views.py
+++ minesweeper/minesweeper_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+#from .forms import RegisterForm
+
 
 # Create your views here.
 def homePageView(request):
