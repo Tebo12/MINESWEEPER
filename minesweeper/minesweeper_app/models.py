85187c456e8f06dceae6e1d26c5df98747cd4342
seryl12 <seryl_12_72@mail.ru>
2025-05-31 15:59:30 +0500

    models

--- minesweeper/minesweeper_app/models.py
+++ minesweeper/minesweeper_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    number_of_titles = models.IntegerField(default=0)
+    number_of_flags = models.IntegerField(default=0)
+
+
+class Results(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    won = models.BooleanField(default=False)
+    time = models.IntegerField(default=0)
