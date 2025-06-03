56bd08afaf1259ddc121d7d42541d8607cdf042e
seryl12 <seryl_12_72@mail.ru>
2025-06-03 18:00:45 +0500

    final

--- minesweeper/minesweeper_app/forms.py
+++ minesweeper/minesweeper_app/forms.py
@@ -9,7 +9,6 @@ class RegisterForm(UserCreationForm):
                              error_messages={
                                  'required': 'Поле email обязательно для заполнения',
                                  'invalid': 'Введите корректный адрес электронной почты',
-                                 'unique': 'Пользователь с таким email уже зарегистрирован'
                              })
 
     class Meta:
@@ -23,43 +22,18 @@ class RegisterForm(UserCreationForm):
                 'required': 'Необходимо указать имя пользователя',
                 'unique': 'Это имя пользователя уже занято',
                 'max_length': 'Имя пользователя не должно превышать 150 символов',
-                'min_length': 'Имя пользователя должно содержать минимум 3 символа',
                 'invalid': 'Имя пользователя содержит недопустимые символы'
-            },
+            }
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        # Убираем подсказки
         self.fields['username'].help_text = ''
         self.fields['password1'].help_text = ''
         self.fields['password2'].help_text = ''
 
-
         self.fields['password1'].label = 'пароль'
         self.fields['password2'].label = 'повтор пароля'
-
-        #Полностью переопределяем сообщения об ошибках для паролей
-        self.fields['password1'].error_messages = {
-            'required': 'Необходимо ввести пароль',
-            'password_too_common': 'Этот пароль слишком распространён',
-            'password_too_short': 'Пароль должен содержать минимум 8 символов',
-            'password_entirely_numeric': 'Пароль не может состоять только из цифр',
-            'password_similar': 'Пароль слишком похож на имя пользователя'
-        }
-
-        self.fields['password2'].error_messages = {
-            'required': 'Необходимо подтвердить пароль',
-            'password_mismatch': 'Введённые пароли не совпадают'  # Это исправляет стандартное сообщение
-        }
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Пароли не совпадают")
-        return password2
 
 
 class LoginForm(AuthenticationForm):
